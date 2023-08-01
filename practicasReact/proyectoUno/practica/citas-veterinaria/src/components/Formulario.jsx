ae7a21e99f718a09b93a4e82d90b792f0b38f67b
Gerardo Ayala Juarez <gera.ay.ju@gmail.com>
2023-08-01 10:00:37 -0600

    Practica tailwind y creando el form

--- practicasReact/proyectoUno/practica/citas-veterinaria/src/components/Formulario.jsx
+++ practicasReact/proyectoUno/practica/citas-veterinaria/src/components/Formulario.jsx
@@ -1,8 +1,28 @@
 
 const Formulario = () => {
   return (
-    <div>
-      <h2>Formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade pacientes y {""}
+        <span className="text-indigo-600 font-bold" >Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5" action="">
+        <div>
+          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+            Nombre Mascota
+            </label>
+
+          <input
+            id="mascota" 
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"  
+            type="text"
+            placeholder="Nombre de la mascota"
+          />
+        </div>
+      </form>
     </div>
   )
 }
