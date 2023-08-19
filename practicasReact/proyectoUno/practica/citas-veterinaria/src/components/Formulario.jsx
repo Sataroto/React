a647e542db15131ab7e4d901a7a9645705446f60
Gerardo Ayala Juarez <gera.ay.ju@gmail.com>
2023-08-18 20:44:27 -0600

    Lista de pacientes

--- practicasReact/proyectoUno/practica/citas-veterinaria/src/components/Formulario.jsx
+++ practicasReact/proyectoUno/practica/citas-veterinaria/src/components/Formulario.jsx
@@ -9,8 +9,8 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold" >Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5" action="">
-        <div>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-5" action="">
+        <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
             Nombre Mascota
             </label>
@@ -22,6 +22,62 @@ const Formulario = () => {
             placeholder="Nombre de la mascota"
           />
         </div>
+        <div className="mb-5">
+          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+            Nombre Propetario
+            </label>
+
+          <input
+            id="propietario" 
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"  
+            type="text"
+            placeholder="Nombre del propietario"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+            email
+            </label>
+
+          <input
+            id="email" 
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"  
+            type="email"
+            placeholder="Direccion email del propietario"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+            Alta
+            </label>
+
+          <input
+            id="alta" 
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"  
+            type="date"
+            placeholder="Fecha de cita"
+          />
+        </div>
+        <div className="mb-5">
+          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+            Sintomas
+            </label>
+          <textarea
+          id="sintomas"
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          placeholder="Describe los sintomas"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all" 
+          value="Agregar paciente"
+
+        
+        />
+
       </form>
     </div>
   )
